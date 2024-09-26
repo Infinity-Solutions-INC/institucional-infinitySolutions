19e0bccaf42af9752c1476faefedff95a394c361
m4yk0n <maykon.rodrigues@sptech.school>
2024-09-25 23:09:49 -0300

    Site institucional conectado ao data-viz - 1 release

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,12 @@
+create database infinitysolutions;
+
+use infinitysolutions;
+
+create table usuario (
+idUsuario int primary key auto_increment,
+nome varchar(50),
+cpf int,
+email varchar(50),
+senha varchar(20)
+);
+
