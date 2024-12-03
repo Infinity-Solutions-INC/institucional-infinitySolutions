59c6840ebc7fb1ca59da8e2452eff6e964084117
lualuaSantosS2 <luana.santos@sptech.school>
2024-12-03 01:46:07 -0300

    subindo interações dos motivos

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var usuarioRouter = require("./src/routes/usuarios");
 var cargoRouter = require("./src/routes/cargo");
 var cursosRouter = require("./src/routes/cursos");
 var turmasRouter = require("./src/routes/turmas");
+var motivoRouter = require("./src/routes/motivo");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,6 +33,7 @@ app.use("/usuarios", usuarioRouter);
 app.use("/cargo", cargoRouter);
 app.use("/cursos", cursosRouter);
 app.use("/turmas", turmasRouter);
+app.use("/motivos", motivoRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
